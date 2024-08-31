dfa328b3f9cfeaa9f778a0fc4c057a821abab2e7
Marcelin <marceloaugusto10123@gmail.com>
2024-08-31 00:16:03 -0300

    feat: create node api to send publish to database

---
+++ app_mqtt_node.js
@@ -0,0 +1,83 @@
+var mqtt = require('mqtt');
+var Topic = '#'; //subscribe to all topics
+var Broker_URL = 'mqtt://hd5088mskf8.sn.mynetname.net:1883';
+var Database_URL = 'hd5088mskf8.sn.mynetname.net:3306';
+
+var options = {
+	clientId: 'MyMQTT',
+	port: 1883,
+	//username: 'mqtt_user',
+	//password: 'mqtt_password',	
+	keepalive : 60
+};
+
+var client  = mqtt.connect(Broker_URL, options);
+client.on('connect', mqtt_connect);
+client.on('reconnect', mqtt_reconnect);
+client.on('error', mqtt_error);
+client.on('message', mqtt_messsageReceived);
+client.on('close', mqtt_close);
+
+function mqtt_connect() {
+    //console.log("Connecting MQTT");
+    client.subscribe(Topic, mqtt_subscribe);
+}
+
+function mqtt_subscribe(err, granted) {
+    console.log("Subscribed to " + Topic);
+    if (err) {console.log(err);}
+}
+
+function mqtt_reconnect(err) {
+    //console.log("Reconnect MQTT");
+    //if (err) {console.log(err);}
+	client  = mqtt.connect(Broker_URL, options);
+}
+
+function mqtt_error(err) {
+    //console.log("Error!");
+	//if (err) {console.log(err);}
+}
+
+function after_publish() {
+	//do nothing
+}
+
+function mqtt_messsageReceived(topic, message, packet) {
+	//console.log('Message received = ' + message);
+	insert_message(topic, message, packet);
+}
+
+function mqtt_close() {
+	//console.log("Close MQTT");
+}
+
+////////////////////////////////////////////////////
+///////////////////// MYSQL ////////////////////////
+////////////////////////////////////////////////////
+var mysql = require('mysql');
+
+//Create Connection
+var connection = mysql.createConnection({
+	host: Database_URL,
+	user: "mqtt",
+	password: "19216801",
+	database: "testes"
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	//console.log("Database Connected!");
+});
+
+//insert a row into the tbl_messages table
+function insert_message(topic, message, packet) {
+	var clientID= "client001";
+	var sql = "INSERT INTO ?? (??,??,??) VALUES (?,?,?)";
+	var params = ['tbl_messages', 'clientID', 'topic', 'message', clientID, topic, message];
+	sql = mysql.format(sql, params);	
+	connection.query(sql, function (error, results) {
+		if (error) throw error;
+		console.log("1 record inserted");
+	});
+};		
